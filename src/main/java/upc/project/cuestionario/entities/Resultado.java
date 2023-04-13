264e7d7d14d43e323d89004081392e59f2f2e1b0
Diego Esquivel <desquivelreuni@hotmail.com>
2023-04-13 16:54:13 -0500

    Tabla usuario creado

---
+++ src/main/java/upc/project/cuestionario/entities/Resultado.java
@@ -0,0 +1,8 @@
+package upc.project.cuestionario.entities;
+
+import jakarta.persistence.Entity;
+
+
+public class Resultado {
+    
+}
--- src/main/java/upc/project/cuestionario/entities/Usuario.java
+++ src/main/java/upc/project/cuestionario/entities/Usuario.java
@@ -1,8 +1,17 @@
 package upc.project.cuestionario.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+// import javax.persistence.*;
 
 @Entity
+@Table(name="usuario")
 public class Usuario {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
