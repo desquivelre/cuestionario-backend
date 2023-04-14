f911fad265f855141d196df2c77ed9682c0bd769
Diego Esquivel <desquivelreuni@hotmail.com>
2023-04-13 20:09:51 -0500

    Se arreglo el problema con el import all

--- src/main/java/upc/project/cuestionario/entities/Usuario.java
+++ src/main/java/upc/project/cuestionario/entities/Usuario.java
@@ -1,12 +1,12 @@
 package upc.project.cuestionario.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.Table;
 
-// import javax.persistence.*;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name="usuario")
